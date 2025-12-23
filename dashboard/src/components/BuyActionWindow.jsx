5b82192ef5bfc0e6ce5602ee00dee2b4581071b2
Aashi Meena <meenaaashi2007@gmail.com>
2025-12-23 14:49:09 +0530

    feat: added dummy data and buyaction window

---
+++ dashboard/src/components/BuyActionWindow.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import GeneralContext from "./GeneralContext";
+import "../BuyActionWindow.css";
+
+const BuyActionWindow = ({ uid }) => {
+
+
+
+    const handleCancelClick = () => {
+        GeneralContext.closeBuyWindow();
+    };
+
+    return (
+        <div className="container" id="buy-window" draggable="true">
+            <div className="regular-order">
+                <div className="inputs">
+                    <fieldset>
+                        <legend>Qty.</legend>
+                        <input
+                            type="number"
+                            name="qty"
+                            id="qty"
+
+                        />
+                    </fieldset>
+                    <fieldset>
+                        <legend>Price</legend>
+                        <input
+                            type="number"
+                            name="price"
+                            id="price"
+                            step="0.05"
+
+                        />
+                    </fieldset>
+                </div>
+            </div>
+
+            <div className="buttons">
+                <span>Margin required ₹140.65</span>
+                <div>
+                    <Link className="buy-btn btns" >
+                        Buy
+                    </Link>
+                    <Link to="" className="cancel-btn btns" onClick={handleCancelClick}>
+                        Cancel
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default BuyActionWindow;
--- dashboard/src/components/WatchList.jsx
+++ dashboard/src/components/WatchList.jsx
@@ -1,4 +1,12 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import { watchlist } from "../data/data";
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import BarChartIcon from '@mui/icons-material/BarChart';
+import Tooltip from '@mui/material/Tooltip';
+import Fade from '@mui/material/Fade';
+import GeneralContext from "./GeneralContext";
 
 const WatchList = () => {
     return (
@@ -11,12 +19,94 @@ const WatchList = () => {
                     placeholder="Search eg:infy, bse, nifty fut weekly, gold mcx"
                     className="search"
                 />
-                <span className="counts"> 9 / 50</span>
+                <span className="counts">{watchlist.length} /50</span>
             </div>
 
-            <ul className="list"></ul>
+            <ul className="list"> {watchlist.map((stock, index) => {
+                return <WatchListItem stock={stock} key={index} />;
+            })}</ul>
+
         </div>
     );
 };
 
 export default WatchList;
+
+const WatchListItem = ({ stock }) => {
+    const [showWatchlistActions, setShowWatchlistActions] = useState(false);
+
+    const handleMouseEnter = (e) => {
+        setShowWatchlistActions(true);
+    };
+
+    const handleMouseLeave = (e) => {
+        setShowWatchlistActions(false);
+    };
+
+    return (
+        <li onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+            <div className="item">
+                <p className={stock.isDown ? "down" : "up"}>{stock.name}</p>
+                <div className="itemInfo">
+                    <span className="percent">{stock.percent}</span>
+                    {stock.isDown ? (
+                        <KeyboardArrowDownIcon className="down" />
+                    ) : (
+                        <KeyboardArrowUpIcon className="up" />
+                    )}
+                    <span className="price">{stock.price}</span>
+                </div>
+            </div>
+            {showWatchlistActions && <WatchListActions uid={stock.name} />}
+        </li>
+    );
+};
+
+
+const WatchListActions = ({ uid }) => {
+
+ const generalContext = useContext(GeneralContext);
+
+  const handleBuyClick = () => {
+    generalContext.openBuyWindow(uid);
+  }
+    return (
+        <span className="actions">
+            <span>
+                <Tooltip
+                    title="Buy (B)"
+                    placement="top"
+                    arrow
+                    TransitionComponent={Fade}
+                  onClick={handleBuyClick}
+                >
+                    <button className="buy">Buy</button>
+                </Tooltip>
+                <Tooltip
+                    title="Sell (S)"
+                    placement="top"
+                    arrow
+                    TransitionComponent={Fade}
+                     onClick={handleBuyClick}
+                >
+                    <button className="sell">Sell</button>
+                </Tooltip>
+                <Tooltip
+                    title="Analytics (A)"
+                    placement="top"
+                    arrow
+                    TransitionComponent={Fade}
+                >
+                    <button className="action">
+                        <BarChartIcon className="icon" />
+                    </button>
+                </Tooltip>
+                <Tooltip title="More" placement="top" arrow TransitionComponent={Fade}>
+                    <button className="action">
+                        <MoreHorizIcon className="icon" />
+                    </button>
+                </Tooltip>
+            </span>
+        </span>
+    )
+};
