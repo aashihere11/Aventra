0c58396003f9d87fd29888658da2c30f9f6eb9b4
Aashi Meena <meenaaashi2007@gmail.com>
2025-12-25 11:37:32 +0530

    connecting-mongoDB

--- backened/Index.js
+++ backened/Index.js
@@ -1,12 +1,18 @@
 const express = require ("express");
-
+const mongoose = require ("mongoose");
+require("dotenv").config();
 const app = express();
-const port = 3000;
+
+const PORT = process.env.PORT || 3000;
+const mongoURL = process.env.MONGO_URL;
+
 
 app.get("/", (req,res) =>{
     res.send("connected")
 });
 
-app.listen(port, () =>{
-    console.log("connected")
+app.listen(PORT, () =>{
+    console.log(`server running on port ${PORT}`);
+    mongoose.connect(mongoURL);
+    console.log("connected to mongoDB");
 });
