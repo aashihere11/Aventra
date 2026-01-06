34efbfafbe072cad5c66e139856d47ce27ebee1e
Aashi Meena <meenaaashi2007@gmail.com>
2026-01-06 12:44:15 +0530

    frontend-ui-change

--- frontend/src/LandingPage/login/Login.jsx
+++ frontend/src/LandingPage/login/Login.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import TextField from '@mui/material/TextField';
 function Login() {
     return (
         <>
@@ -10,37 +11,44 @@ function Login() {
                 <div className="mt-2 border  d-inline-block " style={{ width: "30%", borderRadius: "10px", backgroundColor: "#e2d1ddff" }}>
                     <h2>Login</h2>
                     <form className="mt-3  needs-validation" novalidate>
-                        <div className="mt-3">
-                            <label for="username" class="form-label  px-2">username:</label>
-                            <input
-                                name="username"
-                                type="text"
-                                placeholder="enter username"
-                                id="username"
-                                className="inputs  form-control-sm"
-                                required
-                            />
-                            
-                        </div>
 
-                        <div className="mt-2">
-                            <label for="password" class="form-label px-2">password:</label>
-                            <input
-                                name="password"
-                                type="password"
-                                placeholder="enter password"
-                                id="password"
-                                className="inputs form-control-sm"
-                                required
-                            />
-                        </div>
+
+                        <TextField
+                            id="outlined-basic"
+                            label="username"
+                            variant="outlined"
+                            sx={{
+                                '& .MuiOutlinedInput-root.Mui-focused fieldset': {
+                                    borderColor: 'rgb(172, 144, 163)',
+
+                                },
+                                '& label.Mui-focused': {
+                                    color: 'rgb(172, 144, 163)',
+                                },
+                            }}
+                            className='inputs'
+                        />
+                        <TextField
+                            id="outlined-basic"
+                            label="password"
+                            variant="outlined"
+                            sx={{
+                                '& .MuiOutlinedInput-root.Mui-focused fieldset': {
+                                    borderColor: 'rgb(172, 144, 163)',
+
+                                },
+                                '& label.Mui-focused': {
+                                    color: 'rgb(172, 144, 163)',
+                                },
+                            }}
+                            className='inputs mt-2' />
                         <button type="submit" className="login-button mt-3 ">login</button>
 
                         <div className="login-link m-3 px-5">
                             don't have an account? <Link to="/signup">signup</Link>
                         </div>
                     </form>
-                   
+
                 </div>
             </div>
         </>
